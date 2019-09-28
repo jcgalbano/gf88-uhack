da9bd353b3a0dc51b2d1baa6dcf8e34415ca7772
Jan Carlo Albano <jcgalbano@gmail.com>
2019-09-28 22:48:41 +0800

    Add home and property components and basic routing

---
+++ src/components/property.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Property extends Component {
+  render() {
+    return (
+      <h1>Show Property</h1>
+    )
+  }
+}
+
+export default Property
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/home';
+import Property from './components/property';
+
+const routing = (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/properties/:property_id' component={Property} />
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
